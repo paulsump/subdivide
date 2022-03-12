8976d288b95c8ec5a99a75570ca64f1b27f5333d
paulsump <sumpner@hotmail.com>
2022-03-12 20:16:23 +0000

    using icosahedron first face

--- lib/model/generate.dart
+++ lib/model/generate.dart
@@ -7,14 +7,7 @@ import 'package:subdivide/model/shape_notifier.dart';
 import 'package:subdivide/out.dart';
 import 'package:vector_math/vector_math_64.dart';
 
-ShapeData generateShapeData() => ShapeData(
-      vertices: [
-        Vector3(0, 0, 0),
-        Vector3(1, 0, 0),
-        Vector3(0, 1, 0),
-      ],
-      faces: const [Face(0, 1, 2)],
-    );
+ShapeData generateShapeData() => icosahedron;
 
 const noWarn = out;
 
@@ -30,30 +23,37 @@ const double c2 = (root5 + 1) / 4;
 const double s1 = 0.9510565162951535; //sqrt(10+2*root5)/4;
 const double s2 = 0.5877852522924731; //sqrt(10-2*root5)/4;
 
-class Vertex {
-  final double x, y, z;
-
-  const Vertex(this.x, this.y, this.z);
-}
-
-/// vertices of a unit icosahedron.
-const icosahedronVertices = <Vertex>[
+final icosahedron = ShapeData(
+  vertices: <Vector3>[
 //north pole (z)
-  Vertex(0, 0, root5 / 2),
+    Vector3(0, 0, root5 / 2),
 // top pentagon
-  Vertex(0, 1, 0.5),
-  Vertex(-s1, c1, 0.5),
-  Vertex(-s2, -c2, 0.5),
-  Vertex(s2, -c2, 0.5),
-  Vertex(s1, c1, 0.5),
+    Vector3(0, 1, 0.5),
+    Vector3(-s1, c1, 0.5),
+    Vector3(-s2, -c2, 0.5),
+    Vector3(s2, -c2, 0.5),
+    Vector3(s1, c1, 0.5),
 
-  // bottom pentagon
-  Vertex(0, -1, -0.5),
-  Vertex(-s1, -c1, -0.5),
-  Vertex(-s2, c2, -0.5),
-  Vertex(s2, c2, -0.5),
-  Vertex(s1, -c1, -0.5),
+    // bottom pentagon
+    Vector3(0, -1, -0.5),
+    Vector3(-s1, -c1, -0.5),
+    Vector3(-s2, c2, -0.5),
+    Vector3(s2, c2, -0.5),
+    Vector3(s1, -c1, -0.5),
 
-  // south pole
-  Vertex(0, 0, -root5 / 2),
-];
+    // south pole
+    Vector3(0, 0, -root5 / 2),
+  ],
+  faces: const [
+    Face(0, 1, 2),
+  ],
+);
+
+final triangle = ShapeData(
+  vertices: [
+    Vector3(0, 0, 0),
+    Vector3(1, 0, 0),
+    Vector3(0, 1, 0),
+  ],
+  faces: const [Face(0, 1, 2)],
+);
