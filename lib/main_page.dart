ed2b5b760ed6dcb102b0872cef2ae93f64eec99e
paulsump <sumpner@hotmail.com>
2022-03-12 08:51:24 +0000

    plumbed in panZoomer with gesturer + added horizon

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:subdivide/gesturer.dart';
+import 'package:subdivide/horizon.dart';
+import 'package:subdivide/out.dart';
 import 'package:subdivide/triangle.dart';
 import 'package:subdivide/unit_to_screen.dart';
+import 'package:subdivide/view/hue.dart';
 
+const noWarn = [out];
+
+/// The only page in this app.
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const UnitToScreen(child: Triangle());
+    return Scaffold(
+      body: Container(
+        color: Hue.background,
+        child: SafeArea(
+          left: false,
+          child: Stack(children: const [
+            Horizon(),
+            Gesturer(),
+            UnitToScreen(child: Triangle()),
+          ]),
+        ),
+      ),
+    );
   }
 }
