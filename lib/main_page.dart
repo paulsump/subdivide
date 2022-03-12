8e0864dbf020c32dcf9974ec56573b9bba79244e
paulsump <sumpner@hotmail.com>
2022-03-12 08:42:41 +0000

    triangle on screen

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:subdivide/triangle.dart';
+import 'package:subdivide/unit_to_screen.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const UnitToScreen(child: Triangle());
   }
 }
